bc7a24cbf914b3ee8f5be5cab7518fb6fd91fecb
marcin m <marcinmitruk@gmail.com>
2022-07-31 17:12:21 +0100

    new

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "proxmox" {
   # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = "https://10.225.1.110:8006/api2/json"
+  pm_api_url = "https://192.168.1.161:8006/api2/json"
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "terraform-prov@pve!tokenID123"
+  pm_api_token_id = "terraform-prov@pve!id1234"
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = "7dbf26ae-b062-4ad5-b879-911e3f69152a"
+  pm_api_token_secret = "2b7ab584-8ddd-4193-a5e3-f780daf232d1"
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
 }
@@ -23,11 +23,11 @@ provider "proxmox" {
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "vm-server" {
   count = 0 # just want 1 for now, set to 0 and apply to destroy VM
-  name = "Ubuntu-20.04-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+  name = "Ansible-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = var.proxmox_host
-  desc = "Ubuntu 20.04 clean vm from template"
-  vmid = "11${count.index + 1}" #id for new created vm etc .111, 112, 113
+  desc = "Ubuntu 20.04 clean vm for Ansible"
+  vmid = "10${count.index + 1}" #id for new created vm etc .101, 102, 103
   # oncreate = "false" # Whether to have the VM startup after the PVE node starts.
   # another variable with contents "ubuntu-20.04"
   clone = var.template_name # name of template you want to create from
@@ -46,12 +46,12 @@ resource "proxmox_vm_qemu" "vm-server" {
 
   onboot = "false" # Whether to have the VM startup after the PVE node starts.
 
-  ciuser = "ubuntu"  # Override the default cloud-init user for provisioning.
+  ciuser = "root"  # Override the default cloud-init user for provisioning.
   cipassword = "password" # Override the default cloud-init user's password. Sensitive.
   # automatic_reboot = "true"
 
   vga{
-    type = "std"
+    type = "serial0"
   }
   # if you want two NICs, just copy this whole network section and duplicate it
   network {
@@ -61,9 +61,10 @@ resource "proxmox_vm_qemu" "vm-server" {
 
   disk {
     // This disk will become scsi0
+    ssd = 1
     type = "scsi"
     storage = "local-lvm"
-    size = "10G" # set disk size here. leave it small for testing because expanding the disk takes time.
+    size = "26G" # set disk size here. leave it small for testing because expanding the disk takes time.
     # slot = 0 # (not sure what this is for, seems to be deprecated, do not use).
     iothread = 1 # Whether to use iothreads for this drive. Only effective with a disk of type virtio, or scsi when the the emulated controller type (scsihw top level block argument) is virtio-scsi-single.
   }
@@ -78,7 +79,7 @@ resource "proxmox_vm_qemu" "vm-server" {
   # in this case, since we are only adding a single VM, the IP will
   # be 10.98.1.91 since count.index starts at 0. this is how you can create
   # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
-  ipconfig0 = "ip=10.225.1.11${count.index + 1}/24,gw=10.225.1.1"
+  ipconfig0 = "ip=192.168.1.17${count.index + 1}/24,gw=192.168.1.254"
 
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
